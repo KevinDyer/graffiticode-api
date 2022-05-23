15deeaadee39c5fa901a1c17219f4bfb71baaa32
Kevin Dyer <kevin.m.dyer@gmail.com>
2022-05-22 20:13:48 -0600

    cleanup: use better/cleaner test structure

--- src/routes/task.spec.js
+++ src/routes/task.spec.js
@@ -47,24 +47,32 @@ describe("routes/task", () => {
 
   it("should handle no task ids", async () => {
     await request(app)
-      .get(`/task`)
+      .get("/task")
       .expect(400, createErrorResponse(createError(400, "must provide at least one id")));
   });
 
   it("should get a task that has been created", async () => {
-    await request(app).post("/task").send({ task: TASK1 });
+    const res = await request(app).post("/task").send({ task: TASK1 });
+    expect(res).toHaveProperty("body.status", "success");
+    const id = res.body.data.id;
 
     await request(app)
-      .get(`/task?id=${[TASK_ID1].join(",")}`)
+      .get("/task")
+      .query({ id })
       .expect(200, createSuccessResponse([TASK1]));
   });
 
   it("should get multiple tasks that have been created", async () => {
-    await request(app).post("/task").send({ task: TASK1 });
-    await request(app).post("/task").send({ task: TASK2 });
+    const res1 = await request(app).post("/task").send({ task: TASK1 });
+    expect(res1).toHaveProperty("body.status", "success");
+    const id1 = res1.body.data.id;
+    const res2 = await request(app).post("/task").send({ task: TASK2 });
+    expect(res2).toHaveProperty("body.status", "success");
+    const id2 = res2.body.data.id;
 
     await request(app)
-      .get(`/task?id=${[TASK_ID1, TASK_ID2].join(",")}`)
+      .get("/task")
+      .query({ id: [id1, id2].join(",") })
       .expect(200, createSuccessResponse([TASK1, TASK2]));
   });
 
@@ -77,7 +85,8 @@ describe("routes/task", () => {
     const id = createResponse.body.data.id;
 
     await request(app)
-      .get(`/task?id=${[id].join(",")}`)
+      .get("/task")
+      .query({ id })
       .set("x-graffiticode-storage-type", "firestore")
       .expect(200, createSuccessResponse([TASK1]));
   });
@@ -91,7 +100,8 @@ describe("routes/task", () => {
     const id = createResponse.body.data.id;
 
     await request(app)
-      .get(`/task?id=${[id].join(",")}`)
+      .get("/task")
+      .query({ id })
       .set("x-graffiticode-storage-type", "memory")
       .expect(404);
   });

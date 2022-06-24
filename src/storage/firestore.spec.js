557628a243c6e5b581aa7fd1c35bec7b0e8714b3
Kevin Dyer <kevin.m.dyer@gmail.com>
2022-06-24 11:49:29 -0600

    fix: use firebase admin sdk to talk to firestore

--- src/storage/firestore.spec.js
+++ src/storage/firestore.spec.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const { initializeTestEnvironment } = require('@firebase/rules-unit-testing');
-const { buildFirestoreTaskDao, encodeId } = require('./firestore');
+const { buildFirestoreTaskDao, encodeId, createFirestoreDb } = require('./firestore');
 const { TASK1, TASK2 } = require('../testing/fixture');
 
 describe('storage/firestore', () => {
+  const db = createFirestoreDb({});
+  const taskDao = buildFirestoreTaskDao({ db });
+
   let testEnv = null;
   beforeEach(async () => {
     testEnv = await initializeTestEnvironment({
@@ -24,69 +27,39 @@ describe('storage/firestore', () => {
     }
   });
 
-  const callCreate = async task => {
-    let id;
-    await testEnv.withSecurityRulesDisabled(async context => {
-      const db = context.firestore();
-      const taskDao = buildFirestoreTaskDao({ db });
-      id = await taskDao.create(task);
-    });
-    return id
-  };
-
-  const callGet = async id => {
-    let task;
-    await testEnv.withSecurityRulesDisabled(async context => {
-      const db = context.firestore();
-      const taskDao = buildFirestoreTaskDao({ db });
-      task = await taskDao.get(id);
-    });
-    return task;
-  };
-
-  const callAppendIds = async (id, ...otherIds) => {
-    let newId;
-    await testEnv.withSecurityRulesDisabled(async context => {
-      const db = context.firestore();
-      const taskDao = buildFirestoreTaskDao({ db });
-      newId = await taskDao.appendIds(id, ...otherIds);
-    });
-    return newId;
-  };
-
   it('should throw NotFoundError if task is not created', async () => {
     const id = encodeId({ taskIds: ['foo'] });
 
-    await expect(callGet(id)).rejects.toThrow();
+    await expect(taskDao.get(id)).rejects.toThrow();
   });
 
   it('should create task', async () => {
-    const id = await callCreate(TASK1);
+    const id = await taskDao.create(TASK1);
 
-    await expect(callGet(id)).resolves.toStrictEqual([TASK1]);
+    await expect(taskDao.get(id)).resolves.toStrictEqual([TASK1]);
   });
 
   it('should create tasks', async () => {
-    const id1 = await callCreate(TASK1);
-    const id2 = await callCreate(TASK2);
+    const id1 = await taskDao.create(TASK1);
+    const id2 = await taskDao.create(TASK2);
 
-    await expect(callGet(id1)).resolves.toStrictEqual([TASK1]);
-    await expect(callGet(id2)).resolves.toStrictEqual([TASK2]);
+    await expect(taskDao.get(id1)).resolves.toStrictEqual([TASK1]);
+    await expect(taskDao.get(id2)).resolves.toStrictEqual([TASK2]);
   });
 
   it('should get multi task id', async () => {
-    const id1 = await callCreate(TASK1);
-    const id2 = await callCreate(TASK2);
-    const multiId = await callAppendIds(id1, id2);
+    const id1 = await taskDao.create(TASK1);
+    const id2 = await taskDao.create(TASK2);
+    const multiId = await taskDao.appendIds(id1, id2);
 
-    await expect(callGet(multiId)).resolves.toStrictEqual([TASK1, TASK2]);
+    await expect(taskDao.get(multiId)).resolves.toStrictEqual([TASK1, TASK2]);
   });
 
   it('should get appended task ids', async () => {
-    const id1 = await callCreate(TASK1);
-    const id2 = await callCreate(TASK2);
+    const id1 = await taskDao.create(TASK1);
+    const id2 = await taskDao.create(TASK2);
     const id = `${id1}+${id2}`;
 
-    await expect(callGet(id)).resolves.toStrictEqual([TASK1, TASK2]);
+    await expect(taskDao.get(id)).resolves.toStrictEqual([TASK1, TASK2]);
   });
 });

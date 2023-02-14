0aa12ea402057ffd7e1001b1352b0e3908496895
Jeff Dyer <jeffdyer@acm.org>
2023-02-14 13:10:42 -0800

    fix: restore code to get lang from id with GET /form?id=

--- src/routes/form.js
+++ src/routes/form.js
@@ -2,31 +2,40 @@ import { Router } from "express";
 import { InvalidArgumentError } from "../errors/http.js";
 import { isNonEmptyString } from "../util.js";
 import { buildHttpHandler, optionsHandler } from "./utils.js";
+import { buildGetTasks } from "./task.js";
 
-const buildGetFormHandler = ({ pingLang, getBaseUrlForLanguage }) => () => {
+const checkLangParam = async ({ lang, pingLang }) => {
+  if (/^\d+$/.test(lang)) {
+    lang = `L${lang}`;
+  }
+  if (!/^[Ll]\d+$/.test(lang)) {
+    throw new InvalidArgumentError(`Invalid lang ${lang}`);
+  }
+  if (!await pingLang(lang)) {
+    throw new NotFoundError(`Language not found ${lang}`);
+  }
+};
+
+const buildGetFormHandler = ({ pingLang, getBaseUrlForLanguage }) => ({ taskDaoFactory }) => {
   return buildHttpHandler(async (req, res) => {
     let { id, lang, data } = req.query;
-    if (/^\d+$/.test(lang)) {
-      lang = `L${lang}`;
-    }
-    if (!/^[Ll]\d+$/.test(lang)) {
-      throw new InvalidArgumentError(`invalid lang ${lang}`);
-    }
-    if (!await pingLang(lang)) {
-      res.sendStatus(404);
-      return;
-    }
-    const baseUrl = getBaseUrlForLanguage(lang);
-
     if (isNonEmptyString(id)) {
+      const getTasks = buildGetTasks({ taskDaoFactory, req });
+      const auth = req.auth.context;
+      const tasks = await getTasks({ auth, ids: [id] });
+      const lang = tasks[0].lang;
+      await checkLangParam({ lang, pingLang });
+      const baseUrl = getBaseUrlForLanguage(lang);
       const protocol = baseUrl.indexOf("localhost") !== -1 && "http" || "https";
       const formUrl = `${baseUrl}/form?url=${protocol}://${req.headers.host}/data?id=${id}`;
       res.redirect(formUrl);
     } else if (isNonEmptyString(data)) {
+      await checkLangParam({ lang, pingLang });
+      const baseUrl = getBaseUrlForLanguage(lang);
       const formUrl = `${baseUrl}/form?data=${data}`;
       res.redirect(formUrl);
     } else {
-      res.sendStatus(200);
+      throw new InvalidArgumentError("Missing or invalid parameters");
     }
   });
 };

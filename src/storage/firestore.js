599cb98d1436ac4ecd22e1e3d31b96898f3097e4
Jeff Dyer <jeffdyer@acm.org>
2023-02-27 13:29:50 -0800

    remove debugging

--- src/storage/firestore.js
+++ src/storage/firestore.js
@@ -96,7 +96,6 @@ const buildTaskCreate = ({ db }) => async ({ task, auth }) => {
 
 const buildCheckAuth = () => ({ taskDoc, auth }) => {
   const acls = taskDoc.get("acls");
-  console.log("checkAuth() auth=" + JSON.stringify(auth) + " acls=" + JSON.stringify(acls));
   if (!acls) {
     return;
   }
